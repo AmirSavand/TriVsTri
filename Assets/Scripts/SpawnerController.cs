7e3850a56f18d1016db04f8a670d2a967974f276
Amir Savand <amir@savandbros.com>
2017-10-25 14:17:38 +0400

    Better spawners with custom values

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
 {
+	public float spawnDelay = 0f;
+
+	public float spawnRepeat = 4f;
+
 	public GameObject[] items;
 
 	void Start ()
 	{
 		// Call spawn
-		InvokeRepeating ("spawn", 0f, 1f);
+		InvokeRepeating ("spawn", spawnDelay, spawnRepeat);
 	}
 
 	void spawn ()
 	{
-		// 5% chance of spawn
-		if (Random.value <= 0.25f) {
-			
-			// Get a random item from list
-			GameObject randomItem = items [Random.Range (0, items.Length)];
+		// Get a random item from list
+		GameObject randomItem = items [Random.Range (0, items.Length)];
 
-			// Random spawn point
-			Transform randomPoint = transform.GetChild (Random.Range (0, transform.childCount));
+		// Random spawn point
+		Transform randomPoint = transform.GetChild (Random.Range (0, transform.childCount));
 
-			// Instantiate the item
-			Instantiate (randomItem, randomPoint.transform.position, randomPoint.transform.rotation);
-	
-		}
+		// Instantiate the item
+		Instantiate (randomItem, randomPoint.transform.position, randomPoint.transform.rotation);
 	}
 }
