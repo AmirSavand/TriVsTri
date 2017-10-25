254dac145c816f60e46aaa7f7eb45d2e9f891365
Amir Savand <amir@savandbros.com>
2017-10-25 15:59:06 +0400

    Better wall rotation

--- Assets/Scripts/RandomRotateController.cs
+++ Assets/Scripts/RandomRotateController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RandomRotateController : MonoBehaviour
 {
+	public float minAngle = 0f;
+
+	public float maxAngle = 180f;
 
 	void Start ()
 	{
 		// Set random rotation
-		transform.localRotation = Quaternion.Euler (0f, 0f, Random.Range (0, 180));
+		transform.localRotation = Quaternion.Euler (0f, 0f, Random.Range (minAngle, maxAngle));
 	}
 }
